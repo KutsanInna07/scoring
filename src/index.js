6785e7e6fab4f1a0e1febd75437d427653ba0c98
Inna018 <122346373+Inna018@users.noreply.github.com>
2024-03-28 17:46:01 +0200

    Add useMemo, useCallback

--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useCallback, useContext, useMemo, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import User from "./User";
+import AddingScore from "./AddingScore";
+import Context from "./Context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const ButtonResset = () => {
+  const store = useContext(Context);
+  const { setScore } = store;
+  return (
+    <button
+      className="score-button"
+      onClick={() => {
+        setScore(0);
+      }}
+    >
+      Reset
+    </button>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const CurrentScore = () => {
+  const store = useContext(Context);
+  return <div className="score">{store.score}</div>;
+};
+
+const Score = () => {
+  return (
+    <div className="score-container">
+      <h1>Your Score</h1>
+      <div className="score-wrap">
+        <CurrentScore />
+        <ButtonResset />
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  const [score, setScore] = useState(0);
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+
+  const getGreeting = useCallback((name, surname) => {
+    if (name.length < 3 || surname.length < 3) {
+      return "Hello new user!!!";
+    }
+    const nameToDisplay =
+      name.substring(0, 1).toUpperCase() + name.substring(1, name.length);
+    const surnameToDisplay =
+      surname.substring(0, 1).toUpperCase() + surname.substring(1, name.length);
+    return `Hello ${nameToDisplay} ${surnameToDisplay}!!!`;
+  }, []);
+
+  const addScore = (number) => {
+    setScore(score + number);
+  };
+  const value = {
+    score,
+    setScore,
+    addScore,
+  };
+  const greeting = useMemo(
+    () => getGreeting(name, surname),
+    [name, surname, getGreeting]
+  );
+
+  return (
+    <Context.Provider value={value}>
+      <div className="App">
+        <h1 className="score-container">{greeting}</h1>
+        <Score />
+        <AddingScore />
+        <User
+          name={name}
+          setName={setName}
+          surname={surname}
+          setSurname={setSurname}
+        />
+      </div>
+    </Context.Provider>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
